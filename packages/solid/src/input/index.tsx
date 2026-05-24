1f0bb736c7dbf1f9a117752b1ce9f2d5bce9bc07
fanelfaa <fandi.ilham.bb@gmail.com>
2026-05-24 15:13:19 +0700

    refactor(solid): break input.tsx into input/ directory structure

---
+++ packages/solid/src/input/index.tsx
@@ -0,0 +1,31 @@
+import { Field as ArkField } from "@ark-ui/solid/field";
+import { splitProps, type Component, type JSX } from "solid-js";
+import { InputRoot, InputLabel, InputField, InputDescription, InputErrorText } from "./input.base";
+import { InputVariants } from "@ui/core";
+
+type InputProps = {
+  label?: string;
+  description?: string;
+  class?: string;
+  children?: JSX.Element;
+} & ArkField.InputProps &
+  InputVariants;
+
+export const Input: Component<InputProps> = (props) => {
+  const [local, others] = splitProps(props, ["class", "label", "description", "error", "children"]);
+  return (
+    <InputRoot class={local.class} invalid={!!local.error}>
+      {local.label && <InputLabel>{local.label}</InputLabel>}
+      <InputField error={!!local.error} {...others} />
+      {local.description && !local.error && (
+        <InputDescription>{local.description}</InputDescription>
+      )}
+      {local.error && <InputErrorText>{local.error}</InputErrorText>}
+      {local.children}
+    </InputRoot>
+  );
+};
+
+export * from "./input.base";
+
+export { inputVariants, type InputVariants } from "@ui/core";
