b32355083b40a284987cb4d4787020e51ddca81e
fanelfaa <fandi.ilham.bb@gmail.com>
2026-05-24 15:13:30 +0700

    refactor(solid): break pin-input.tsx into pin-input/ directory structure

---
+++ packages/solid/src/pin-input/index.tsx
@@ -0,0 +1,7 @@
+import { PinInputRoot } from "./pin-input.base";
+
+export const PinInput = PinInputRoot;
+
+export * from "./pin-input.base";
+
+export { pinInputVariants, type PinInputVariants } from "@ui/core";
