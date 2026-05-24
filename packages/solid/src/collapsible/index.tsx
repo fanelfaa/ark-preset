05020ba9c0e2e7d115379ddd903fea7a1cd89514
fanelfaa <fandi.ilham.bb@gmail.com>
2026-05-24 14:23:26 +0700

    ref: remove .sisypus and change styled. to base.

--- packages/solid/src/collapsible/index.tsx
+++ packages/solid/src/collapsible/index.tsx
@@ -1,7 +1,7 @@
-import { CollapsibleRoot } from "./collapsible.styled";
+import { CollapsibleRoot } from "./collapsible.base";
 
 export const Collapsible = CollapsibleRoot;
 
-export * from "./collapsible.styled";
+export * from "./collapsible.base";
 
 export { collapsibleVariants, type CollapsibleVariants } from "@ui/core";
