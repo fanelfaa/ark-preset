40ea8fa7beba0b5cd643b707d04ef90b336ef3cc
Fandi Ilham <fandi@zero-one-group.com>
2026-07-18 13:05:42 +0700

    style(docs): inline route component in form page

--- apps/docs/src/routes/docs/integrations/form.tsx
+++ apps/docs/src/routes/docs/integrations/form.tsx
@@ -2,9 +2,5 @@ import { createFileRoute } from "@tanstack/solid-router";
 import TanstackFormDoc from "../../../content/docs/tanstack-form/index.tsx";
 
 export const Route = createFileRoute("/docs/integrations/form")({
-  component: RouteComponent,
+  component: () => <TanstackFormDoc />,
 });
-
-function RouteComponent() {
-  return <TanstackFormDoc />;
-}
