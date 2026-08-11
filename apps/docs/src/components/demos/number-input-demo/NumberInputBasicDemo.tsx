3a0d40d2cfbf09393dc9df8703ca94e29bc267d8
Fandi Ilham <fandi@zero-one-group.com>
2026-08-11 14:46:11 +0700

    docs(number-input): implement ComponentPreview and component page updates

--- apps/docs/src/components/demos/number-input-demo/NumberInputBasicDemo.tsx
+++ apps/docs/src/components/demos/number-input-demo/NumberInputBasicDemo.tsx
@@ -1,10 +1,9 @@
 import { NumberInput } from "@ark-preset/solid";
-import { DemoWrapper } from "../../DemoWrapper";
 
 export default function NumberInputBasicDemo() {
   return (
-    <DemoWrapper>
+    <div>
       <NumberInput defaultValue="50" min={0} max={100} />
-    </DemoWrapper>
+    </div>
   );
 }
