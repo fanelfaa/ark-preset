316a35d13fb15c98ebabc471eee9d2cb67b1be0a
Fandi Ilham <fandi@zero-one-group.com>
2026-08-11 14:46:28 +0700

    docs(radio-group): implement ComponentPreview and component page updates

--- apps/docs/src/components/demos/radio-group-demo/RadioGroupDisabledDemo.tsx
+++ apps/docs/src/components/demos/radio-group-demo/RadioGroupDisabledDemo.tsx
@@ -1,5 +1,4 @@
 import { Index } from "solid-js";
-import { DemoWrapper } from "../../DemoWrapper";
 import { RadioGroup, RadioGroupItem } from "@ark-preset/solid";
 const paymentMethods = [
   { value: "1", label: "Credit Card" },
@@ -8,7 +7,7 @@ const paymentMethods = [
 ];
 export default function RadioGroupDisabledDemo() {
   return (
-    <DemoWrapper>
+    <div>
       <RadioGroup defaultValue="1" orientation="horizontal">
         <Index each={paymentMethods}>
           {(method) => (
@@ -18,6 +17,6 @@ export default function RadioGroupDisabledDemo() {
           )}
         </Index>
       </RadioGroup>
-    </DemoWrapper>
+    </div>
   );
 }
